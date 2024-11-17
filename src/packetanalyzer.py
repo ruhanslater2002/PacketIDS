dccbca9e67f7872e9cb3777780e1f714fbbe6145
Ruhan <ruhan.slater@gmail.com>
2024-11-17 14:38:49 +0200

    updated old log remove

--- src/packetanalyzer.py
+++ src/packetanalyzer.py
@@ -11,15 +11,16 @@ class PacketAnalyzer:
         self.time_window: float = time_window
         self.traffic_logs: dict = {}
 
-    def get_or_create_log(self, source_ip: str) -> dict:
+    def get_or_create_log(self, source_ip: str, current_time: float) -> dict:
         # Retrieve existing traffic log or create a new one
         if source_ip not in self.traffic_logs:
-            self.traffic_logs[source_ip] = {'timestamp': 0, 'ports': set()}
+            self.traffic_logs[source_ip] = {'timestamp': current_time, 'ports': set()}  # Creates a log with the current time stamp
         return self.traffic_logs[source_ip]
 
     def update_log(self, log: dict, current_time: float, dest_port: int, source_ip: str) -> None:
         # Remove old timestamps that are outside the time window
         if current_time - log['timestamp'] > self.time_window:
+            self.logger.info(f"Log for {colored(source_ip, "yellow")} has been cleared, out of time window {colored(int(self.time_window), "yellow")}.")
             self.traffic_logs.pop(source_ip, None)
             return
         # Update timestamps and ports in the traffic log
@@ -44,7 +45,7 @@ class PacketAnalyzer:
                 dst_port: int = packet[scapy.TCP].dport
                 current_time: float = time.time()
                 # Get or create traffic log for the source IP
-                traffic_log: dict = self.get_or_create_log(source_ip)
+                traffic_log: dict = self.get_or_create_log(source_ip, current_time)
                 self.update_log(traffic_log, current_time, dst_port, source_ip)
                 self.detect_port_scan(traffic_log, source_ip, dst_port)
         except Exception as e:
